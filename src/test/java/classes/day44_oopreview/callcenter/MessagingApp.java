e540a06d5d96d6af51df664d739712e6f8831a58
AsudEsma <asesozdemir@gmail.com>
2022-01-21 13:34:49 +0300

    Second part of the review is written.

---
+++ src/test/java/classes/day44_oopreview/callcenter/MessagingApp.java
@@ -0,0 +1,31 @@
+package classes.day44_oopreview.callcenter;
+
+public abstract class MessagingApp {
+    public String name;
+    protected boolean isFree;
+    boolean allOSCompatible;
+    private static int count;
+    public static final String APP_TYPE = "Messanger";
+
+    public MessagingApp() {
+        System.out.println("MessangerApp no-args constructor");
+    }
+
+    public abstract void SendMessage(String msg);
+
+    public void Launch() {
+        System.out.println("The app is launching...");
+    }
+
+    public static void close() {
+        System.out.println("The app is closing...");
+    }
+
+    public static void setCount(int count) {
+        MessagingApp.count = count;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+}
