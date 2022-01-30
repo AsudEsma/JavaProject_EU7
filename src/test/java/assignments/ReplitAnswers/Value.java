44b011057d4ddb3f1b3a48992c675b6e7f45294e
AsudEsma <asesozdemir@gmail.com>
2022-01-30 18:16:46 +0300

    Value repl.it task is here.

---
+++ src/test/java/assignments/ReplitAnswers/Value.java
@@ -0,0 +1,28 @@
+package assignments.ReplitAnswers;
+
+public class Value {
+    private int val;
+    private boolean modified;
+
+    public Value() {
+    }
+
+    public Value(int val) {
+        this.val = val;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        if(val!=this.val) {
+            this.val=val;
+            modified = true;
+        }
+    }
+
+    public boolean wasModified() {
+        return modified;
+    }
+}
