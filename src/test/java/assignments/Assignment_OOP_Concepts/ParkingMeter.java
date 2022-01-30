430f08a42a6c80773af16cd409a8e8ce946a09c7
AsudEsma <asesozdemir@gmail.com>
2022-01-30 17:59:08 +0300

    ParkingMeter repl.it task is here.

---
+++ src/test/java/assignments/Assignment_OOP_Concepts/ParkingMeter.java
@@ -0,0 +1,33 @@
+package assignments.Assignment_OOP_Concepts;
+
+public class ParkingMeter {
+    int timeLeft = 0;
+    private int maxTime;
+
+    public ParkingMeter(int maxTime) {
+        this.maxTime = maxTime;
+    }
+
+    public boolean add(int num) {
+        if(num==25) {
+            if(timeLeft+25 < maxTime) {
+                timeLeft += 30;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void tick() {
+        if(timeLeft>0) {
+            timeLeft--;
+        }
+    }
+
+    public boolean isExpired() {
+        if(timeLeft==0) {
+            return true;
+        }
+        return false;
+    }
+}
